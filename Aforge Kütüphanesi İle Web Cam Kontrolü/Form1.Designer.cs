c6caeff551b532bbcf2a9883bdbce20e20edad51
Kadir11619 <82710596+Kadir11619@users.noreply.github.com>
2023-08-18 00:05:50 +0300

    Add project files.

---
+++ Aforge Kütüphanesi İle Web Cam Kontrolü/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Aforge_Kütüphanesi_İle_Web_Cam_Kontrolü
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(49, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kameralar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(502, 338);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(178, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(494, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Yakala";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(544, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(352, 329);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 423);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Aforge Kütüphanesi İle Web Cam Kontrolü/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+namespace Aforge_Kütüphanesi_İle_Web_Cam_Kontrolü
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection fico; // Bilgisayara bağlı kameraları tutan dizi
+        VideoCaptureDevice vcd;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fico = new FilterInfoCollection(FilterCategory.VideoInputDevice);// Bilgisayara bağlı kameraları getiriyor. 
+            foreach (FilterInfo f in fico)
+            {
+                comboBox1.Items.Add(f.Name); // Kamera ismini ekle 
+                comboBox1.SelectedIndex = 0; // seçili olan değer 0 index olsun, kamerayı seçmeden direkt kendi seçiyor. 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vcd = new VideoCaptureDevice(fico[comboBox1.SelectedIndex].MonikerString);
+            vcd.NewFrame += Vcd_NewFrame; // Bu olsuşan metot kameraya çerçeveyi tanıtıcaz 
+            vcd.Start();
+            //timer1.Start(); 
+        }
+
+        private void Vcd_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone(); // Bitmap sınıfından ilgili pictureboxa kameradan aldıgımız çerceveyi aktarıyoruz. 
+        }
+        //  Kameredan fotoğraf Yakalama işlemi 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "(*.jpg)|*.jpg";
+            DialogResult dr = s.ShowDialog(); //Dosya  açtırma işlemi 
+            if (dr == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(s.FileName);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*
+            if (pictureBox1 != null)
+            {
+                BarcodeReader brd = new BarcodeReader();
+                Result sonuc = brd.Decode((Bitmap)pictureBox1.Image);
+                if (sonuc != null)
+                {
+                    richTextBox1.Text = sonuc.ToString();
+                    timer1.Stop();
+                }
+            }
+            */ 
+            // Yukarıdaki işlem telefondan barkod gösterdiğimiz zaman barkodu okuyor ve richtextboza aktarıyor fakat null değer diye hata alıyorum o yüzden kapatattım . 
+        }
+    }
+}
